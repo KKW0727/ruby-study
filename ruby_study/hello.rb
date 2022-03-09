64df62d424fee70e0de4fbbd149b847c039fe9e3
kkw0727 <kkw3401@gmaiil.com>
2022-03-09 16:14:43 +0900

    add: if

--- ruby_study/hello.rb
+++ ruby_study/hello.rb
@@ -253,6 +253,21 @@ sprintf
 実行結果
 "明日、甲子園の最高気温は29度です。"
 =end 
-p sprintf("name: %10s\n", "taguchi")
+p sprintf("name: %10s\n", "aimyon")
 p sprintf("id: %05d, rate: %10.2f\n", 355, 3.284)
 
+#if
+#> < >= <= == !=
+## &&(AND) ||(OR) !(NOT)
+puts "点数を入力してください"
+# gets / ユーザーから入力を受け付けるメソッド　
+score = gets.to_i #getsで受け取るのは文字列になるからそれを数値に変換する必要がある
+ if score > 90 then
+     puts "よく頑張ったな!"
+ elsif score > 80 then
+     puts "あと少しだね"
+ else 
+     puts "もっと頑張りなさい!"
+ end
+
+#puts "よく頑張ったな!" if score > 95 #単純な条件分岐の場合使える
